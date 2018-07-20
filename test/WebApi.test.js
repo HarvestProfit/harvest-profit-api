768cd5bad1232dcbc27b1093f8a26a091d25e21c
Chris Bernard <chris@harvestprofit.com>
2018-07-20 09:42:36 -0500

    added authenticated get tests

--- test/WebApi.test.js
+++ test/WebApi.test.js
@@ -15,10 +15,36 @@ describe('Webapi', () => {
       //make a request here
       let params = {
         name: 'Harvest',
-        reason: 'fun'
+        email: 'harvest@harvestprofit.com'
       }
       moxios.wait(function () {
         let request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: [
+            { id: 1, item: 'Harvest-item1' },
+            { id: 2, item: 'Harvest-item2' }
+          ]
+        })
+      });
+      WebApi.get('/endpoint', params)
+      .then(function (response) {
+        expect(response.data.status).toEqual(200);
+      });
+    });
+
+    it('it should make an authenticated get request', () => {
+      WebApi.setAuthCookie('harvestprofitauth');
+      let params = {
+        name: 'Harvest',
+        email: 'harvest@harvestprofit.com'
+      };
+      let headers = {
+        Authorization: WebApi.getAuthCookie()
+      };
+      moxios.wait(function () {
+        let request = moxios.requests.mostRecent()
+        if (request.headers.Authentication == 'harvestprofitauth') {
         request.respondWith({
           status: 200,
           response: [
@@ -26,17 +52,49 @@ describe('Webapi', () => {
             { id: 2, item: 'carrot', cooked: true }
           ]
         })
+      } else {
+        request.respondWith({
+          status: 401
+        })
+      }
       });
-      WebApi.get('/endpoint', params)
+      WebApi.get('/secureEndpoint', params, headers)
       .then(function (response) {
-        console.log(response);
         expect(response.data.status).toEqual(200);
-      });
+      })
     });
+
+    it('it should fail to make an authenticated get request', () => {
+      WebApi.setAuthCookie('harvestprofitauth');
+      let params = {
+        name: 'Harvest',
+        email: 'harvest@harvestprofit.com'
+      };
+      let headers = {
+        Authorization: 'notrighttoken'
+      };
+      moxios.wait(function () {
+        let request = moxios.requests.mostRecent()
+        if (request.headers.Authentication == 'harvestprofitauth') {
+        request.respondWith({
+          status: 200,
+          response: [
+            { id: 1, item: 'potato', cooked: false },
+            { id: 2, item: 'carrot', cooked: true }
+          ]
+        })
+      } else {
+        request.respondWith({
+          status: 401
+        })
+      }
+      });
+      WebApi.get('/secureEndpoint', params, headers)
+      .then(function (response) {
+        expect(response.data.status).toEqual(401);
+      })
+    });
+
+    
   });
 });
-describe('tests', () => {
-    it('it should run this test', () => {
-      expect(1).toEqual(1);
-    });
-  });
