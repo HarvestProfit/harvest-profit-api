72f9e1fdf890a55ce3c33bbe67ba62dc36409997
Chris Bernard <chris@harvestprofit.com>
2018-07-24 09:41:34 -0500

    move authenticated to api

--- src/WebApi.js
+++ src/WebApi.js
@@ -29,101 +29,6 @@ eslint no-underscore-dangle: ["error", {
 class WebApi {
   constructor() {
     this.baseUrl = process.env.API_URL || 'http://localhost:3000/api/v3';
-    this.interceptor = this._setInteceptor();
-  }
-
-  /**
-   * Setup interceptor to refresh tokens if necessary. Only useful for
-   * authenticated API requests.
-   * @private
-   */
-  _setInteceptor() {
-    axios.interceptors.response.use(response => response, (error) => {
-      const original = error ? error.config : { url: '' };
-      let responseCode = 0;
-      if (error && error.response) {
-        responseCode = error.response.status;
-      }
-
-      if (original && original.url.indexOf('refresh') === -1 && !original.harvest_retried && responseCode === 403) {
-        original.harvest_retried = true;
-        return new Promise((resolve, reject) => {
-          this._getAuthenticated('/refresh').then((response) => {
-            original.headers.Authorization = response.data.auth_token;
-            return resolve(axios(original));
-          }).catch((exception) => {
-            return reject(exception);
-          });
-        });
-      }
-      return Promise.reject(error);
-    });
-  }
-
-  /**
-   * Performs an authenticated get request on the specified Url with optional
-   * parameters
-   * @private
-   * @param {string} url The url to make a request to
-   * @param {object} params The params to add to the url
-   */
-  _getAuthenticated(url, params = {}) {
-    const apiUrl = this.baseUrl + url;
-    return axios.get(apiUrl, {
-      params,
-      headers: {
-        Authorization: WebApi.getAuthCookie(),
-      },
-    });
-  }
-
-  /**
-   * Performs an authenticated post request on the specified Url with optional
-   * parameters
-   * @private
-   * @param {string} url The url to make a request to
-   * @param {object} data The data to post
-   */
-  _postAuthenticated(url, data = {}) {
-    const apiUrl = this.baseUrl + url;
-    return axios.post(apiUrl, data, {
-      headers: {
-        Authorization: WebApi.getAuthCookie(),
-      },
-    });
-  }
-
-  /**
-   * Performs an authenticated put request on the specified Url with optional
-   * parameters
-   * @private
-   * @param {string} url The url to make a request to
-   * @param {object} data The data to post
-   */
-  _putAuthenticated(url, data = {}) {
-    const apiUrl = this.baseUrl + url;
-    return axios.put(apiUrl, data, {
-      headers: {
-        Authorization: WebApi.getAuthCookie(),
-      },
-    });
-  }
-
-  /**
-   * Performs an authenticated delete request on the specified Url with optional
-   * parameters
-   * @private
-   * @param {string} url The url to make a delete request to
-   * @param {object} params The params to add to the url
-   */
-  _deleteAuthenticated(url, params = {}) {
-    const apiUrl = this.baseUrl + url;
-    return axios.delete(apiUrl, {
-      params,
-      headers: {
-        Authorization: WebApi.getAuthCookie(),
-      },
-    });
   }
 
   /**
@@ -145,7 +50,7 @@ class WebApi {
    * @return {object}
    */
   static getAuthenticated(url, params = {}) {
-    const api = new WebApi();
+    const api = new Api(this.baseUrl, this.getAuthCookie);
     return api._getAuthenticated(url, params);
   }
 
@@ -168,7 +73,7 @@ class WebApi {
    * @return {object}
    */
   static postAuthenticated(url, data = {}) {
-    const api = new WebApi();
+    const api = new Api(this.baseUrl, this.getAuthCookie);
     return api._postAuthenticated(url, data);
   }
 
@@ -191,7 +96,7 @@ class WebApi {
    * @return {object}
    */
   static putAuthenticated(url, data = {}) {
-    const api = new WebApi();
+    const api = new Api(this.baseUrl, this.getAuthCookie);
     return api._putAuthenticated(url, data);
   }
 
@@ -214,7 +119,7 @@ class WebApi {
    * @return {object}
    */
   static deleteAuthenticated(url, params = {}) {
-    const api = new WebApi();
+    const api = new Api(this.baseUrl, this.getAuthCookie);
     return api._deleteAuthenticated(url, params);
   }
 
